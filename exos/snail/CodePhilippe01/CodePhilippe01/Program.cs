830b4ca692bcd1833d2117547e8c70e5ab3e76a3
philippe-hjik <philippe.heijkoop@eduvaud.ch>
2023-08-30 10:35:22 +0200

    Snail Begin
    Full code

---
+++ exos/snail/CodePhilippe01/CodePhilippe01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CodePhilippe01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SNAIL_POS_Y = 15;
+            const int SNAIL_LIFE = 50;
+
+            for (int i = 0; i != SNAIL_LIFE; i++)
+            {
+                Console.SetCursorPosition(i, SNAIL_POS_Y);
+                Thread.Sleep(39);
+
+                Snail(0);
+            }
+            Console.SetCursorPosition(50, SNAIL_POS_Y);
+            Snail(1);
+
+            Console.ReadLine();
+
+            void Snail(int Pos)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                if (Pos == 0)
+                    Console.Write(" _@_ö");
+                else if (Pos == 1)
+                    Console.Write(" ____");
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
